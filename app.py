255f4d9be304c39acc8a67afa99321a2e731a685
edmarsanches1982-code <edmar.sanches1982@gmail.com>
2025-12-15 11:44:50 -0100

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ if dados:
     df = carregar_dados(dados)
     st.table(df)
 
-    else:
-      st.info("carregue um ficheiro excel para começar")
+ else:
+     st.info("carregue um ficheiro excel para começar")
     
 
     
