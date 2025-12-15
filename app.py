731fcbb7cc44f3437cf193e161b76cdf84308e5d
edmarsanches1982-code <edmar.sanches1982@gmail.com>
2025-12-15 12:15:54 -0100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-#import numpy as np
+import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
-#import time
-#import re
+import matplotlib.pyplot as plt
+import time
+import re
 import openpyxl
-#from datetime import date
+from datetime import date
 st.header("Introduzindo os elementos do streamlit")
 menu = option_menu(menu_title="menu",
                    options=["Início","Gráficos Estátisticos", "Gráficos Dinamicos", "wiggets","Formulario"],
                    icons=["house", "bar-chat","bar-chat-line", "toggles","bar-chat"],
                    menu_icon="cast",
                    default_index=0,
-                   orientation="horizontal"
-                   
+                   orientation="horizontal"                 
   )
 with st.sidebar:
     st.success("**UPLOAD DE DADOS**")
