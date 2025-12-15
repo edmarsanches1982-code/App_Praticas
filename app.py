f2e92e322313db7e09b12897fecbd32744438fb7
edmarsanches1982-code <edmar.sanches1982@gmail.com>
2025-12-15 11:14:33 -0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+import re
+fom datetime import date
 st.header("Introduzindo os elementos do streamlit")
 menu = option_menu(menu_title="menu",
                    options=["Início","Gráficos Estátisticos", "Gráficos Dinamicos", "wiggets","Formulario"],
