3248cbaa2b1dedea1e33a941d0d9d9a9dccc7ac6
edmarsanches1982-code <edmar.sanches1982@gmail.com>
2025-12-15 11:05:48 -0100

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import streamlit as st
 st.header("Introduzindo os elementos do streamlit")
+menu = option_menu(menu_title="menu",
+                   options=["Início","Gráficos Estátisticos", "Gráficos Dinamicos", "wiggets","Formulario"],
+                   icons=["house", "bar-chat","bar-chat-line", "toggles","bar-chat"],
+                   menu_icon="cast",
+                   default_index=0,
+                   orientation="horizontal"
+                   
+  )
