439f10c7239e25368e4e83a87595449a552e7717
edmarsanches1982-code <edmar.sanches1982@gmail.com>
2025-12-15 11:54:38 -0100

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ if dados:
           return df
         except FileNotfoundError:
           return pd.dataframe()
-  df = carregar_dados(dados)
-  st.table(df)
+    df = carregar_dados(dados)
+    st.table(df)
 else:
   st.info("carregue um ficheiro excel para começar")
     
