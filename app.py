4cb753d065e397d4368751514981e2faa8f32407
edmarsanches1982-code <edmar.sanches1982@gmail.com>
2025-12-15 12:20:36 -0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from datetime import date
 st.header("Introduzindo os elementos do streamlit")
 menu = option_menu(menu_title="menu",
                    options=["Início","Gráficos Estátisticos", "Gráficos Dinamicos", "wiggets","Formulario"],
-                   icons=["house", "bar-chat","bar-chat-line", "toggles","bar-chat"],
+                   icons=["house", "bar-chart","bar-chart-line", "toggles","bar-chart"],
                    menu_icon="cast",
                    default_index=0,
                    orientation="horizontal"                 
@@ -26,8 +26,8 @@ if dados:
           try:
             df =pd.read_excel(dados)
             return df
-          except FileNotfoundError:
-            return pd.dataframe()
+          except FilefoundError:
+            return pd.DataFrame()
             df = carregar_dados(dados)
             st.table(df)
 else:
